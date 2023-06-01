f2114d586a6a056d0f7a3e246f19d99ef23e954f
VitorTG <vitor.tavares@aluno.ufop.edu.br>
2023-06-01 10:43:36 -0300

    Aqrquivos feitos

---
+++ README.md
@@ -0,0 +1,1 @@
+Primeiro teste feito em JS, criei uma função e um novo programa para validar essa função, curso feito pelo processo seletivo TerraLab
---
+++ teste.teste.js
@@ -0,0 +1,14 @@
+describe('sum test', () => {
+  beforeAll(async () => {
+  console.log('Before all')
+  });
+  beforeEach(async () => {
+  console.log('Before each');
+  });
+  afterAll(async () => {
+  console.log('after all');
+  });
+  afterEach(async () => {
+  console.log('after each');
+  });
+  });
